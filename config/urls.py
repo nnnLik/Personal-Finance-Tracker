4ed94f0ca3e465eeeb6d1f85c54b4e7b73997721
nnnLik <makhmudov_ra@vk.com>
2023-02-04 18:44:19 +0300

    [ADD] base route and pre auth

---
+++ config/urls.py
@@ -0,0 +1,18 @@
+"""config URL Configuration"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+
+from config import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('api-auth/', include('rest_framework.urls')),
+    # path('auth/', include('djoser.urls')),
+    # path('auth/', include('djoser.urls.jwt')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
