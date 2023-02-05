b3d9f0bd229b31386b4a61f071b3b45762cfab1f
nnnLik <makhmudov_ra@vk.com>
2023-02-05 19:54:45 +0300

    [ADD] user app, exclude from admin field

---
+++ src/user/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+
+from django.contrib.auth.admin import UserAdmin
+from .models import UserFinance
+
+from django.utils.translation import gettext, gettext_lazy as _
+
+
+class UserFinanceAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "phone",
+        "first_login",
+    )
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": ("is_active",),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (_("Info"), {"fields": ("phone", "avatar", "gender")}),
+    )
+
+
+admin.site.register(UserFinance, UserFinanceAdmin)
