1e784ced3a505ac5a145ac25e269bcc39f109aa1
nnnLik <makhmudov_ra@vk.com>
2023-02-18 19:29:03 +0300

    [EDIT] test app

--- config/api.py
+++ config/api.py
@@ -1,8 +1,6 @@
 from ninja import NinjaAPI
 from src.user.api import router as user_router
-from src.post.api import router as post_router
 
 api = NinjaAPI()
 
 api.add_router("/user/", user_router)
-api.add_router("/post/", post_router)
